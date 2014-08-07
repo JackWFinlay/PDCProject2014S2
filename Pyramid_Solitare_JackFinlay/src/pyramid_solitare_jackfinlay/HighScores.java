ef50f2a5367c5ae40912b26cd63450eea00aec56
JackWFinlay <Jack.Witbrock@gmail.com>
2014-08-07 14:03:10 +1200

    Created

---
+++ Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/HighScores.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pyramid_solitare_jackfinlay;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Jack Finlay ID: 1399273
+ */
+public class HighScores {
+    
+    ArrayList<Score> highScores;
+    
+    public void writeScoreFile(){
+        
+    }
+    
+}
---
+++ Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/Score.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pyramid_solitare_jackfinlay;
+
+/**
+ *
+ * @author Jack Finlay ID: 1399273
+ */
+public class Score {
+    private int score;
+    private String playerName;
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    
+    
+}
