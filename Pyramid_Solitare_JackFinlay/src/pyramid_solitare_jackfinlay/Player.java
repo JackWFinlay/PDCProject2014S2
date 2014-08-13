cbca63db8e6af2243ced97d0281e8205c5acc59c
JackWFinlay <Jack.Witbrock@gmail.com>
2014-08-13 12:09:21 +1200

    Renamed from "Scores" to "Player".

---
+++ Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/Player.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pyramid_solitare_jackfinlay;
+
+/**
+ *
+ * @author Jack Finlay ID: 1399273
+ */
+public class Player {
+    private int score;
+    private String playerName;
+    
+    public Player(){}
+    
+    public Player( String playerName ){
+        this.playerName = playerName;
+        this.score = 0;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+    
+    
+}
