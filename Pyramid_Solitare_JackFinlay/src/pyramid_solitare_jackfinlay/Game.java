4470ecac80898e6c23b7b6d9761e7f4e9b51ffc2
JackWFinlay <Jack.Witbrock@gmail.com>
2014-08-14 14:17:07 +1200

    Added selectCard() and setSelected() methods.

--- Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/Game.java
+++ Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/Game.java
@@ -6,26 +6,52 @@
 
 package pyramid_solitare_jackfinlay;
 
+import static pyramid_solitare_jackfinlay.CUI.scanner;
+
 /**
  *
  * @author Jack Finlay ID: 1399273
  */
 public final class Game {
     public static Player player;
+    public static Board board;
+    public static Card selectedCard1, selectedCard2;
     
     public Game(Player player){
-        
+        selectedCard1 = null;
+        selectedCard2 = null;
         Game.player = player;
+        
         newGame();
     }
     
     public void newGame(){
         Deck deck = new Deck();
         deck.createDeck();
-        Board board = new Board(deck);
+        board = new Board(deck);
         board.printUI();
         
-        
+    }
+    
+    public void continueGame() {
+        board.printUI();
+    }
+    
+    public void selectCard(){
+        System.out.println("Enter a pile (Board - b, Pickup - p, Waste - w \n>");
+        String pile = scanner.next();
+        if ( pile.equals("P") || pile.equals("p")) {
+            //setSelected(); TODO
+        }
         
     }
+    
+    public void setSelected(Card card){
+        if (selectedCard1 == null){
+            selectedCard1 = card;
+        } else {
+            selectedCard2 = card;
+        }
+    }
+    
 }
