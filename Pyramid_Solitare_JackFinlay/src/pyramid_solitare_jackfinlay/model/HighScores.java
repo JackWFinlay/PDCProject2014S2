92592a0b9e87d66d684b036917ad02408565e8a6
Jack <Jack@Jack-Laptop>
2014-10-01 17:25:12 +1300

    Commented out while other parts of code are being developed.

--- Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/model/HighScores.java
+++ Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/model/HighScores.java
@@ -1,156 +1,156 @@
-package pyramid_solitare_jackfinlay.model;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static pyramid_solitare_jackfinlay.Main.cui;
-import pyramid_solitare_jackfinlay.model.ui.CUI;
-import static pyramid_solitare_jackfinlay.model.ui.CUI.scanner;
-
-/**
- * Reads, maintains, and writes the saved high scores file.
- *
- * @author Jack Finlay ID: 1399273
- */
-public final class HighScores {
-
-    private static final String SCORES_FILE_LOCATION = "../Data/Scores.txt";
-    private ArrayList<Score> highScores;
-
-    /**
-     * Constructor to set up HighScores instance.
-     */
-    public HighScores() {
-        highScores = new ArrayList();
-        readScoreFile();
-    }
-
-    /**
-     * Reads the score file stored in the .jar file.
-     */
-    private void readScoreFile() {
-
-        try {
-
-            InputStream in = getClass().getResourceAsStream(SCORES_FILE_LOCATION);
-            // Using InputStream rather than FileReader allows storing of 
-            // txt file in the .jar file
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            String line;
-
-            while ((line = reader.readLine()) != null) {
-
-                StringTokenizer tokenizer = new StringTokenizer(line, ",");
-                int rank = Integer.parseInt(tokenizer.nextToken());
-                int score = Integer.parseInt(tokenizer.nextToken());
-                String name = (tokenizer.nextToken());
-
-                highScores.add(new Score(rank, name, score));
-
-            }
-
-            in.close();
-
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(HighScores.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(HighScores.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Updates the current score roster.
-     *
-     * @param player The current player.
-     */
-    public void updateHighScores(Player player) {
-        int lowestIndex = highScores.size();
-        Score currentPlayer;
-        currentPlayer = new Score(0, player.getPlayerName(), player.getScore());
-
-        for (int i = (highScores.size() - 1); i >= 0; i--) {
-            if (currentPlayer.getScore() > highScores.get(i).getScore()) {
-                lowestIndex = i;
-            }
-        } // Find if player beat a high score.
-
-        if (highScores.size() >= 10 || lowestIndex <= highScores.size()) {
-            highScores.add(lowestIndex, currentPlayer);
-            // Add player at index - index of 10 puts it at rank 11.
-        } else {
-            highScores.add(currentPlayer);
-            // If there aren't 10 highscores and it doesn't beat any, 
-            // stick on end of list.
-        }
-
-        for (Score score : highScores) {
-            score.setRank((highScores.indexOf(score)) + 1);
-        } // Resest ranks
-
-        if (highScores.size() > 10) {
-            highScores.remove(10); // Remove score at rank 11.
-        }
-    }
-
-    /**
-     * Writes the current high score list to a .txt file.
-     */
-    public void writeScoreFile() {
-        PrintWriter output = null;
-
-        URL url = getClass().getResource(SCORES_FILE_LOCATION);
-
-        try {
-            output = new PrintWriter(new FileOutputStream(url.getPath()));
-        } catch (FileNotFoundException ex) {
-            Logger.getLogger(HighScores.class.getName()).log(Level.SEVERE, null, ex);
-        }
-
-        if (output != null) {
-
-            for (Score score : highScores) {
-                int rank = score.getRank();
-                int playerScore = score.getScore();
-                String name = score.getPlayerName();
-
-                String outputLine = (rank + "," + playerScore + "," + name);
-                output.println(outputLine);
-            }
-
-            output.close();
-        }
-    }
-
-    /**
-     * Prints the current high score list to the console.
-     */
-    public void printHighScores() {
-
-        System.out.println("Rank   Name        Score");
-
-        for (Score score : highScores) {
-
-            System.out.print(score.getRank());
-            System.out.print("      " + score.getPlayerName());
-            System.out.print("      " + score.getScore());
-            System.out.println("");
-
-        }
-
-        System.out.println("Press the enter key to return to menu...");
-        scanner.nextLine(); // Clear scanner buffer.
-
-        CUI.clearConsole();
-        cui.menu();
-    }
-
-}
+//package pyramid_solitare_jackfinlay.model;
+//
+//import java.io.BufferedReader;
+//import java.io.FileNotFoundException;
+//import java.io.FileOutputStream;
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.io.InputStreamReader;
+//import java.io.PrintWriter;
+//import java.net.URL;
+//import java.util.ArrayList;
+//import java.util.StringTokenizer;
+//import java.util.logging.Level;
+//import java.util.logging.Logger;
+//import static pyramid_solitare_jackfinlay.Main.cui;
+//import pyramid_solitare_jackfinlay.model.ui.CUI;
+//import static pyramid_solitare_jackfinlay.model.ui.CUI.scanner;
+//
+///**
+// * Reads, maintains, and writes the saved high scores file.
+// *
+// * @author Jack Finlay ID: 1399273
+// */
+//public final class HighScores {
+//
+//    private static final String SCORES_FILE_LOCATION = "../Data/Scores.txt";
+//    private ArrayList<Score> highScores;
+//
+//    /**
+//     * Constructor to set up HighScores instance.
+//     */
+//    public HighScores() {
+//        highScores = new ArrayList();
+//        readScoreFile();
+//    }
+//
+//    /**
+//     * Reads the score file stored in the .jar file.
+//     */
+//    private void readScoreFile() {
+//
+//        try {
+//
+//            InputStream in = getClass().getResourceAsStream(SCORES_FILE_LOCATION);
+//            // Using InputStream rather than FileReader allows storing of 
+//            // txt file in the .jar file
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+//            String line;
+//
+//            while ((line = reader.readLine()) != null) {
+//
+//                StringTokenizer tokenizer = new StringTokenizer(line, ",");
+//                int rank = Integer.parseInt(tokenizer.nextToken());
+//                int score = Integer.parseInt(tokenizer.nextToken());
+//                String name = (tokenizer.nextToken());
+//
+//                highScores.add(new Score(rank, name, score));
+//
+//            }
+//
+//            in.close();
+//
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(HighScores.class.getName()).log(Level.SEVERE, null, ex);
+//        } catch (IOException ex) {
+//            Logger.getLogger(HighScores.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//    }
+//
+//    /**
+//     * Updates the current score roster.
+//     *
+//     * @param player The current player.
+//     */
+//    public void updateHighScores(Player player) {
+//        int lowestIndex = highScores.size();
+//        Score currentPlayer;
+//        currentPlayer = new Score(0, player.getPlayerName(), player.getScore());
+//
+//        for (int i = (highScores.size() - 1); i >= 0; i--) {
+//            if (currentPlayer.getScore() > highScores.get(i).getScore()) {
+//                lowestIndex = i;
+//            }
+//        } // Find if player beat a high score.
+//
+//        if (highScores.size() >= 10 || lowestIndex <= highScores.size()) {
+//            highScores.add(lowestIndex, currentPlayer);
+//            // Add player at index - index of 10 puts it at rank 11.
+//        } else {
+//            highScores.add(currentPlayer);
+//            // If there aren't 10 highscores and it doesn't beat any, 
+//            // stick on end of list.
+//        }
+//
+//        for (Score score : highScores) {
+//            score.setRank((highScores.indexOf(score)) + 1);
+//        } // Resest ranks
+//
+//        if (highScores.size() > 10) {
+//            highScores.remove(10); // Remove score at rank 11.
+//        }
+//    }
+//
+//    /**
+//     * Writes the current high score list to a .txt file.
+//     */
+//    public void writeScoreFile() {
+//        PrintWriter output = null;
+//
+//        URL url = getClass().getResource(SCORES_FILE_LOCATION);
+//
+//        try {
+//            output = new PrintWriter(new FileOutputStream(url.getPath()));
+//        } catch (FileNotFoundException ex) {
+//            Logger.getLogger(HighScores.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//
+//        if (output != null) {
+//
+//            for (Score score : highScores) {
+//                int rank = score.getRank();
+//                int playerScore = score.getScore();
+//                String name = score.getPlayerName();
+//
+//                String outputLine = (rank + "," + playerScore + "," + name);
+//                output.println(outputLine);
+//            }
+//
+//            output.close();
+//        }
+//    }
+//
+//    /**
+//     * Prints the current high score list to the console.
+//     */
+//    public void printHighScores() {
+//
+//        System.out.println("Rank   Name        Score");
+//
+//        for (Score score : highScores) {
+//
+//            System.out.print(score.getRank());
+//            System.out.print("      " + score.getPlayerName());
+//            System.out.print("      " + score.getScore());
+//            System.out.println("");
+//
+//        }
+//
+//        System.out.println("Press the enter key to return to menu...");
+//        scanner.nextLine(); // Clear scanner buffer.
+//
+//        CUI.clearConsole();
+//        cui.menu();
+//    }
+//
+//}
--- Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/model/ui/CUI.java
+++ Pyramid_Solitare_JackFinlay/src/pyramid_solitare_jackfinlay/model/ui/CUI.java
@@ -1,234 +1,234 @@
-package pyramid_solitare_jackfinlay.model.ui;
-
-import java.util.Scanner;
-import java.util.StringTokenizer;
-import pyramid_solitare_jackfinlay.model.Card;
-import pyramid_solitare_jackfinlay.model.Game;
-import pyramid_solitare_jackfinlay.model.Help;
-import pyramid_solitare_jackfinlay.model.HighScores;
-import pyramid_solitare_jackfinlay.model.Player;
-
-/**
- * Manages input and output to and from the game.
- *
- * @author Jack Finlay ID: 1399273
- */
-public class CUI {
-
-    public static Scanner scanner = new Scanner(System.in);
-    public static Game game;
-
-    /**
-     * The default constructor.
-     */
-    public CUI() {
-    }
-
-    /**
-     * Prints the menu and manages menu related activities.
-     */
-    public void menu() {
-        String input;
-        int selection = 0;
-
-        System.out.println("Welcome to Pyramid Solitaire\n");
-        System.out.println("Menu:");
-        System.out.println("1 - New Game");
-        System.out.println("2 - Highscores");
-        System.out.println("3 - Exit");
-
-        do {
-            boolean validInput = false;
-
-            while (!validInput) { // Check that the input is an integer
-                System.out.print("\nEnter a selection (number) \n > ");
-
-                input = scanner.nextLine();
-
-                try {
-                    selection = Integer.parseInt(input.trim());
-
-                    validInput = true;
-                } catch (NumberFormatException error) {
-                    validInput = false;
-
-                    selection = 4;
-                }
-            }
-
-            if ((selection < 1 || selection > 3)) {
-                System.out.println("Invalid input. Please try again.");
-            }
-
-        } while (selection < 1 || selection > 3);
-
-        if (selection == 1) {
-            newGame();
-
-        } else if (selection == 2) {
-
-            clearConsole();
-            HighScores hs = new HighScores();
-            hs.printHighScores();
-
-        } else if (selection == 3) {
-
-            System.exit(0);
-        }
-    }
-
-    /**
-     * Sets up a new game and runs it.
-     */
-    public void newGame() {
-        String playerName;
-        String input;
-
-        do {
-            System.out.print("Enter your name, or type \"Cancel\" to abort. \n > ");
-            playerName = scanner.nextLine();
-
-            if (playerName.equalsIgnoreCase("Cancel")) { //Player chose to cancel
-                clearConsole();
-                menu();
-            }
-
-            System.out.print("Is \'" + playerName + "\' correct? (Y/N) \n> ");
-
-            input = scanner.nextLine();
-
-            if (!input.equalsIgnoreCase("Y")) {
-                playerName = "";
-            }
-
-        } while (playerName.isEmpty()); // Name cannot be an empty line
-
-        game = new Game(new Player(playerName));
-
-        while (true) {
-
-            System.out.print("Enter a command, or type help\n> ");
-            input = scanner.nextLine();
-            commandInterpretter(input);
-        }
-    }
-
-    public static void clearConsole() {
-        for (int i = 0; i < 50; i++) {
-            System.out.println("");
-        } //Simpler than implementing platform specific console clears.
-    }
-
-    /**
-     * Parses and executes the commands entered by the player.
-     *
-     * @param command The command to be interpreted.
-     */
-    public void commandInterpretter(String command) {
-
-        command = command.toLowerCase();
-
-        if (command.equals("help")) {
-
-            showHelp();
-        } else if (command.equals("exit")) {
-
-            showExit();
-        } else if (command.startsWith("select")) {
-
-            showSelect(command);
-        } else if (command.equals("unselect")) {
-
-            System.out.println("Card unselected.");
-            game.setSelectedCard1(null);
-
-        } else if (command.equals("draw")) {
-
-            game.getBoard().draw();
-            game.continueGame();
-
-        } else if (command.equals("shuffle")) {
-
-            showShuffle();
-        } else {
-            System.out.println("Invalid input. Try again.");
-        }
-    }
-
-    /**
-     * Prompts confirmation to exit and prints out high scores.
-     */
-    private void showExit() {
-        System.out.print("Are you sure? (Y/N)\n> ");
-        String command = scanner.nextLine();
-
-        if (command.equalsIgnoreCase("Y")) {
-            System.out.println("Final Score: "
-                    + game.getPlayer().getScore());
-            System.out.println("");
-            HighScores hs = new HighScores();
-            hs.updateHighScores(game.getPlayer());
-            hs.writeScoreFile();
-            hs.printHighScores();
-
-            menu();
-
-        } else {
-            game.continueGame();
-        }
-    }
-
-    /**
-     * Handles tasks related to the 'Select' command.
-     *
-     * @param command The command string to read the card name from.
-     */
-    private void showSelect(String command) {
-        StringTokenizer tokenizer = new StringTokenizer(command, " ");
-        tokenizer.nextToken();
-
-        if (tokenizer.hasMoreTokens()) { // Check card name is presesnt
-
-            String card = tokenizer.nextToken();
-            Card selectedCard = game.selectCard(card);
-
-            if (selectedCard != null) {
-                game.setSelected(selectedCard);
-            }
-
-        } else {
-            System.out.println("Select command requires a card name.");
-        }
-
-        if (game.getSelectedCard2() != null) {
-            game.checkMatch(game.getSelectedCard1(), game.getSelectedCard2());
-            game.continueGame();
-        }
-    }
-
-    /**
-     * Handles tasks related to the 'Shuffle' command.
-     */
-    private void showShuffle() {
-        if (game.getShufflesRemaining() > 0) {
-
-            game.decrementShufflesRemaining();
-            game.newBoard();
-
-        } else {
-            System.out.println("Cannot reshuffle. Type 'Exit' to end game.");
-        }
-    }
-
-    /**
-     * Handles tasks related to the 'help' command.
-     */
-    private void showHelp() {
-        Help help = new Help();
-
-        System.out.println("Press return key to continue...");
-        String consumeBuffer = scanner.nextLine();
-
-        game.continueGame();
-    }
-}
+//package pyramid_solitare_jackfinlay.model.ui;
+//
+//import java.util.Scanner;
+//import java.util.StringTokenizer;
+//import pyramid_solitare_jackfinlay.model.Card;
+//import pyramid_solitare_jackfinlay.model.Game;
+//import pyramid_solitare_jackfinlay.model.Help;
+//import pyramid_solitare_jackfinlay.model.HighScores;
+//import pyramid_solitare_jackfinlay.model.Player;
+//
+///**
+// * Manages input and output to and from the game.
+// *
+// * @author Jack Finlay ID: 1399273
+// */
+//public class CUI {
+//
+//    public static Scanner scanner = new Scanner(System.in);
+//    public static Game game;
+//
+//    /**
+//     * The default constructor.
+//     */
+//    public CUI() {
+//    }
+//
+//    /**
+//     * Prints the menu and manages menu related activities.
+//     */
+//    public void menu() {
+//        String input;
+//        int selection = 0;
+//
+//        System.out.println("Welcome to Pyramid Solitaire\n");
+//        System.out.println("Menu:");
+//        System.out.println("1 - New Game");
+//        System.out.println("2 - Highscores");
+//        System.out.println("3 - Exit");
+//
+//        do {
+//            boolean validInput = false;
+//
+//            while (!validInput) { // Check that the input is an integer
+//                System.out.print("\nEnter a selection (number) \n > ");
+//
+//                input = scanner.nextLine();
+//
+//                try {
+//                    selection = Integer.parseInt(input.trim());
+//
+//                    validInput = true;
+//                } catch (NumberFormatException error) {
+//                    validInput = false;
+//
+//                    selection = 4;
+//                }
+//            }
+//
+//            if ((selection < 1 || selection > 3)) {
+//                System.out.println("Invalid input. Please try again.");
+//            }
+//
+//        } while (selection < 1 || selection > 3);
+//
+//        if (selection == 1) {
+//            newGame();
+//
+//        } else if (selection == 2) {
+//
+//            clearConsole();
+//            HighScores hs = new HighScores();
+//            hs.printHighScores();
+//
+//        } else if (selection == 3) {
+//
+//            System.exit(0);
+//        }
+//    }
+//
+//    /**
+//     * Sets up a new game and runs it.
+//     */
+//    public void newGame() {
+//        String playerName;
+//        String input;
+//
+//        do {
+//            System.out.print("Enter your name, or type \"Cancel\" to abort. \n > ");
+//            playerName = scanner.nextLine();
+//
+//            if (playerName.equalsIgnoreCase("Cancel")) { //Player chose to cancel
+//                clearConsole();
+//                menu();
+//            }
+//
+//            System.out.print("Is \'" + playerName + "\' correct? (Y/N) \n> ");
+//
+//            input = scanner.nextLine();
+//
+//            if (!input.equalsIgnoreCase("Y")) {
+//                playerName = "";
+//            }
+//
+//        } while (playerName.isEmpty()); // Name cannot be an empty line
+//
+//        game = new Game(new Player(playerName));
+//
+//        while (true) {
+//
+//            System.out.print("Enter a command, or type help\n> ");
+//            input = scanner.nextLine();
+//            commandInterpretter(input);
+//        }
+//    }
+//
+//    public static void clearConsole() {
+//        for (int i = 0; i < 50; i++) {
+//            System.out.println("");
+//        } //Simpler than implementing platform specific console clears.
+//    }
+//
+//    /**
+//     * Parses and executes the commands entered by the player.
+//     *
+//     * @param command The command to be interpreted.
+//     */
+//    public void commandInterpretter(String command) {
+//
+//        command = command.toLowerCase();
+//
+//        if (command.equals("help")) {
+//
+//            showHelp();
+//        } else if (command.equals("exit")) {
+//
+//            showExit();
+//        } else if (command.startsWith("select")) {
+//
+//            showSelect(command);
+//        } else if (command.equals("unselect")) {
+//
+//            System.out.println("Card unselected.");
+//            game.setSelectedCard1(null);
+//
+//        } else if (command.equals("draw")) {
+//
+//            game.getBoard().draw();
+//            game.continueGame();
+//
+//        } else if (command.equals("shuffle")) {
+//
+//            showShuffle();
+//        } else {
+//            System.out.println("Invalid input. Try again.");
+//        }
+//    }
+//
+//    /**
+//     * Prompts confirmation to exit and prints out high scores.
+//     */
+//    private void showExit() {
+//        System.out.print("Are you sure? (Y/N)\n> ");
+//        String command = scanner.nextLine();
+//
+//        if (command.equalsIgnoreCase("Y")) {
+//            System.out.println("Final Score: "
+//                    + game.getPlayer().getScore());
+//            System.out.println("");
+//            HighScores hs = new HighScores();
+//            hs.updateHighScores(game.getPlayer());
+//            hs.writeScoreFile();
+//            hs.printHighScores();
+//
+//            menu();
+//
+//        } else {
+//            game.continueGame();
+//        }
+//    }
+//
+//    /**
+//     * Handles tasks related to the 'Select' command.
+//     *
+//     * @param command The command string to read the card name from.
+//     */
+//    private void showSelect(String command) {
+//        StringTokenizer tokenizer = new StringTokenizer(command, " ");
+//        tokenizer.nextToken();
+//
+//        if (tokenizer.hasMoreTokens()) { // Check card name is presesnt
+//
+//            String card = tokenizer.nextToken();
+//            Card selectedCard = game.selectCard(card);
+//
+//            if (selectedCard != null) {
+//                game.setSelected(selectedCard);
+//            }
+//
+//        } else {
+//            System.out.println("Select command requires a card name.");
+//        }
+//
+//        if (game.getSelectedCard2() != null) {
+//            game.checkMatch(game.getSelectedCard1(), game.getSelectedCard2());
+//            game.continueGame();
+//        }
+//    }
+//
+//    /**
+//     * Handles tasks related to the 'Shuffle' command.
+//     */
+//    private void showShuffle() {
+//        if (game.getShufflesRemaining() > 0) {
+//
+//            game.decrementShufflesRemaining();
+//            game.newBoard();
+//
+//        } else {
+//            System.out.println("Cannot reshuffle. Type 'Exit' to end game.");
+//        }
+//    }
+//
+//    /**
+//     * Handles tasks related to the 'help' command.
+//     */
+//    private void showHelp() {
+//        Help help = new Help();
+//
+//        System.out.println("Press return key to continue...");
+//        String consumeBuffer = scanner.nextLine();
+//
+//        game.continueGame();
+//    }
+//}
